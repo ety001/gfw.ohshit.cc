5e6062a7ceac311db7023fa1f8ed45a70ca094ce
ETY001 <ety001@domyself.me>
2015-02-26 14:15:41 +0800

    new files

--- controller/main.php
+++ controller/main.php
@@ -2,5 +2,11 @@
 class main extends spController
 {
 	public function index(){
+        $page               = array(
+            'title'     => '私人定制'
+        );
+        $css_js['head_css'] = array('res/css/global.css');
+        $this->page         = $page;
+        tpl_display($this, 'index.html', $css_js);
     }
 }
